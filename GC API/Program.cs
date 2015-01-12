f1b061161d4f1ab5c56ee2521c0d5dcf972cdff2
AndBoTheCat <andbogc@gmail.com>
2015-01-13 00:19:24 +0400

    Repetition
    User's nickname repeat added. Some bugs fixed.

--- GC API/Program.cs
+++ GC API/Program.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EasyHttp.Http;
+using System.Threading;
 
 namespace GC_API
 {
     class player
     {
         static string result;
-        public player (string a)
+        public player(string a)
         {
             result = a;
         }
@@ -20,7 +21,7 @@ namespace GC_API
             string a = "";
             while (result[i + 10] != '\n')
             {
-                a = a +result[i + 10];
+                a = a + result[i + 10];
                 i++;
             }
             return a;
@@ -35,10 +36,10 @@ namespace GC_API
                 a = a + result[i + 6];
                 i++;
             }
-            if(a=="true")
+            if (a == "true")
                 return true;
             else
-                return false;   
+                return false;
         }
 
         public string date()
@@ -64,10 +65,10 @@ namespace GC_API
             {
                 a = a + result[i + 6];
                 i++;
-            }   
+            }
             int Usertime = int.Parse(a);
             int NowTime = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
-            if (NowTime - Usertime <= aktivTime )
+            if (NowTime - Usertime <= aktivTime)
                 return true;
             else
                 return false;
@@ -77,15 +78,15 @@ namespace GC_API
         {
             int i = result.IndexOf("[");
             string a = "";
-            while (result[i-1] != ']')
+            while (result[i - 1] != ']')
             {
                 a = a + result[i];
                 i++;
             }
             if (a == "[]")
                 a = "";
-            if (a=="[&aHELP!&4]")
-            a="[HELP!]";
+            if (a == "[&aHELP!&4]")
+                a = "[HELP!]";
             if (a == "[&1MOD&9]")
                 a = "[MOD]";
             if (a == "[&rff66c016G&rfff7f7f7C&r99446666]")
@@ -121,7 +122,7 @@ namespace GC_API
             string time = Convert.ToString(pDate);
             return time;
         }
-}
+    }
 
 
     class Program
@@ -131,7 +132,7 @@ namespace GC_API
 
             var http = new HttpClient();
             http.Request.Accept = HttpContentTypes.ApplicationJson;
-            string url = "https://api.greencubes.org" + type + name;
+            string url = "https://api.greencubes.org" + type + "/"+ name;
             var result = http.Get(url);
             string ans = Convert.ToString(result.RawText);
             ans = ans.Replace("{", "");
@@ -141,10 +142,10 @@ namespace GC_API
             return ans;
         }
 
-        static void output(string type, string name)
+        static void output(string res)
         {
-            player user = new player(get(type, name));
-            Console.Write("\n"+user.prefix());
+            player user = new player(res);
+            Console.Write("\n" + user.prefix());
             Console.WriteLine(user.name());
             if (user.status() == true)
             {
@@ -177,16 +178,27 @@ namespace GC_API
             }
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Игрок зарегистрирован {0}", user.reg_date());
- 
+
         }
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White;
             string type = "/users";
             Console.WriteLine("Введите ник пользователя:");
-            string name = "/" + Console.ReadLine();
-            output(type, name);
-            Console.ReadKey();
+            string name = Console.ReadLine();
+            while(name!="Y")
+            {
+            string res = get(type, name);
+            if (res.IndexOf("message") == -1)
+            {
+                output(res);
+            }
+            else
+                Console.WriteLine("Такого игрока не существует.");
+            Console.WriteLine("Вы можете ввести ник ещё раз или выйти (Y).");
+            //Console.WriteLine(res.IndexOf("message"));
+            name = Console.ReadLine();
+            }
         }
     }
 }
